ed98e18f1f3390d685f56ff05cc0fae37c193425
Haobijam Chetan Singh <67411987+chetanhaobijam@users.noreply.github.com>
2023-09-07 14:34:24 +0530

    Learned Inserting & Retrieving Images
    Learned a topic about Inserting and Retrieving Images from SQL Database in ASP.NET

---
+++ InsertImages.aspx
@@ -0,0 +1,71 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="InsertImages.aspx.cs" Inherits="ASP_Tutorial.InsertImages" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Inserting and Retrieving Images Exercise</title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 69%;
+            height: 156px;
+        }
+        .auto-style2 {
+            width: 322px;
+        }
+        .auto-style3 {
+            width: 127px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <table cellpadding="4" cellspacing="4" class="auto-style1">
+                <tr>
+                    <td class="auto-style3">Image Upload</td>
+                    <td class="auto-style2">
+                        <asp:FileUpload ID="FileUpload1" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style3">&nbsp;</td>
+                    <td class="auto-style2">
+                        <asp:Button ID="BtnSubmit" OnClick="BtnSubmit_Click" runat="server" Height="27px" Text="Submit" Width="82px" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style3">&nbsp;</td>
+                    <td class="auto-style2">
+                        <asp:Label ID="Label1" runat="server" Visible="False"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" ShowHeader="false">
+                            <Columns>
+                                <asp:TemplateField>
+                                    <ItemTemplate>
+                                        <table>
+                                            <tr>
+                                                <td>
+                                                    <asp:Label ID="LblImage" runat="server" Text='<%#Eval("ImageId") %>'></asp:Label>
+                                                </td>
+                                                <td>
+                                                    <asp:Image ID="Image1" runat="server" ImageUrl='<%#Eval("ImageName") %>' Width="300" />
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                            </Columns>
+                        </asp:GridView>
+                    </td>
+                </tr>
+            </table>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ InsertImages.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Data;
+
+namespace ASP_Tutorial
+{
+    public partial class InsertImages : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetImage();
+            }
+            if(IsPostBack)
+            {
+                GetImage();
+            }
+        }
+
+        protected void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string path = Server.MapPath("images/");
+            if(FileUpload1.HasFile)
+            {
+                string fileName = Path.GetFileName(FileUpload1.FileName);
+                string extension = Path.GetExtension(fileName);
+                HttpPostedFile postedFile = FileUpload1.PostedFile;
+                int length = postedFile.ContentLength;
+                if(extension == ".jpg" || extension == ".png" || extension == ".jpeg")
+                {
+                    if(length <= 1000000)
+                    {
+                        FileUpload1.SaveAs(path + fileName);
+                        string name = "images/" + fileName;
+
+                        string query = "Insert into Images values(@img)";
+                        SqlCommand cmd = new SqlCommand(query, con);
+                        cmd.Parameters.AddWithValue("@img", name);
+                        con.Open();
+                        int a = cmd.ExecuteNonQuery();
+                        if(a  > 0)
+                        {
+                            Label1.Text = "Inserted Successfully";
+                            Label1.ForeColor = Color.Green;
+                            Label1.Visible = true;
+                            GetImage();
+                        } else
+                        {
+                            Label1.Text = "Inserted failed";
+                            Label1.ForeColor = Color.Red;
+                            Label1.Visible = true;
+                        }
+                    } else
+                    {
+                        Label1.Text = "The images size should not be greater than 1MB";
+                        Label1.ForeColor = Color.Red;
+                        Label1.Visible = true;
+                    }
+                } else
+                {
+                    Label1.Text = "Image Format not supported";
+                    Label1.ForeColor = Color.Red;
+                    Label1.Visible = true;
+                }
+
+            }
+        }
+
+        protected void GetImage()
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string query = "Select * from Images";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
