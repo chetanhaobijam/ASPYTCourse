86eabbce63b4eebe1f32cd4642dc78dca59c54ee
Haobijam Chetan Singh <67411987+chetanhaobijam@users.noreply.github.com>
2023-10-20 15:59:42 +0530

    AutoIncrement value
    Learned an ASP Course topic about autoincrementing value in text field after inserting to Database

---
+++ AutoIncrement.aspx
@@ -0,0 +1,89 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AutoIncrement.aspx.cs" Inherits="ASP_Tutorial.AutoIncrement" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Autoincrement Value</title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 400px;
+        }
+        .auto-style2 {
+            width: 92px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table cellpadding="4" cellspacing="4" class="auto-style1">
+                <tr>
+                    <td class="auto-style2">ID</td>
+                    <td>
+                        <asp:TextBox ID="Txt_Id" runat="server" Height="26px" Width="278px" ReadOnly="True"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Name</td>
+                    <td>
+                        <asp:TextBox ID="Txt_Name" runat="server" Height="26px" Width="278px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Age</td>
+                    <td>
+                        <asp:TextBox ID="Txt_Age" runat="server" Height="26px" Width="278px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">&nbsp;</td>
+                    <td>
+                        <asp:Button ID="Btn_Insert" runat="server" Height="36px" Text="Insert" Width="77px" OnClick="Btn_Insert_Click" />
+&nbsp;
+                        <asp:Button ID="Btn_Update" runat="server" Height="36px" Text="Update" Width="77px" OnClick="Btn_Update_Click" />
+&nbsp;
+                        <asp:Button ID="Btn_Delete" runat="server" Height="36px" Text="Delete" Width="77px" OnClick="Btn_Delete_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" AutoGenerateColumns="False" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+                            <AlternatingRowStyle BackColor="White" />
+                            <Columns>
+                                <asp:CommandField ShowSelectButton="True" HeaderText="SELECT"></asp:CommandField>
+                                <asp:TemplateField HeaderText="ID">
+                                    <ItemTemplate>
+                                        <asp:Label ID="Lbl_Id" runat="server" Text='<%# Eval("id") %>'></asp:Label>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="NAME">
+                                    <ItemTemplate>
+                                        <asp:Label ID="Lbl_Name" runat="server" Text='<%# Eval("name") %>'></asp:Label>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="AGE">
+                                    <ItemTemplate>
+                                        <asp:Label ID="Lbl_Age" runat="server" Text='<%# Eval("age") %>'></asp:Label>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                            </Columns>
+
+                            <EditRowStyle BackColor="#2461BF" />
+                            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                            <RowStyle BackColor="#EFF3FB" />
+                            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+                        </asp:GridView>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AutoIncrement.aspx.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ASP_Tutorial
+{
+    public partial class AutoIncrement : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                GetId();
+                BindGridView();
+            }
+        }
+
+        protected void GetId()
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string query1 = "select id from autoincrement";
+            SqlDataAdapter sda = new SqlDataAdapter(query1, con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if(dt.Rows.Count < 1 )
+            {
+                Txt_Id.Text = "1";
+            }
+            else
+            {
+                string query2 = "select max(id) from autoincrement";
+                SqlCommand cmd = new SqlCommand(query2, con);
+                con.Open();
+                int i = Convert.ToInt32(cmd.ExecuteScalar());
+                con.Close();
+                i = i + 1;
+                Txt_Id.Text = i.ToString();
+            }
+        }
+
+        protected void Btn_Insert_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string query = "insert into autoincrement values(@id, @name, @age)";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@id", Txt_Id.Text);
+            cmd.Parameters.AddWithValue("@name", Txt_Name.Text);
+            cmd.Parameters.AddWithValue("@age", Txt_Age.Text);
+            con.Open();
+            int a = cmd.ExecuteNonQuery();
+            if(a > 0)
+            {
+                Response.Write("<script>alert('Data inserted')</script>");
+                GetId();
+                BindGridView();
+                Txt_Name.Text = "";
+                Txt_Age.Text = "";
+            }
+            else
+            {
+                Response.Write("<script>alert('Data not inserted')</script>");
+            }
+        }
+
+        protected void BindGridView()
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string query1 = "select * from autoincrement";
+            SqlDataAdapter sda = new SqlDataAdapter(query1, con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow row = GridView1.SelectedRow;
+            Label lbl_Id = (Label)row.FindControl("Lbl_Id");
+            Label lbl_Name = (Label)row.FindControl("Lbl_Name");
+            Label lbl_Age = (Label)row.FindControl("Lbl_Age");
+            Txt_Id.Text = lbl_Id.Text;
+            Txt_Name.Text= lbl_Name.Text;
+            Txt_Age.Text= lbl_Age.Text;
+
+        }
+
+        protected void Btn_Update_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string query = "update autoincrement set name = @name, age = @age where id = @id";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@id", Txt_Id.Text);
+            cmd.Parameters.AddWithValue("@name", Txt_Name.Text);
+            cmd.Parameters.AddWithValue("@age", Txt_Age.Text);
+            con.Open();
+            int a = cmd.ExecuteNonQuery();
+            if (a > 0)
+            {
+                Response.Write("<script>alert('Data updated')</script>");
+                GetId();
+                BindGridView();
+                Txt_Name.Text = "";
+                Txt_Age.Text = "";
+            }
+            else
+            {
+                Response.Write("<script>alert('Data not updated')</script>");
+            }
+        }
+
+        protected void Btn_Delete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string query = "delete from autoincrement where id = @id";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@id", Txt_Id.Text);
+            con.Open();
+            int a = cmd.ExecuteNonQuery();
+            if (a > 0)
+            {
+                Response.Write("<script>alert('Data deleted')</script>");
+                GetId();
+                BindGridView();
+                Txt_Name.Text = "";
+                Txt_Age.Text = "";
+            }
+            else
+            {
+                Response.Write("<script>alert('Data not deleted')</script>");
+            }
+        }
+    }
+}
