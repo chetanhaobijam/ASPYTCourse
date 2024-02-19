9edac6c44bda7eec6f4a7240f92b5f66aacde9eb
Haobijam Chetan Singh <67411987+chetanhaobijam@users.noreply.github.com>
2024-02-19 15:30:39 +0530

    Bind DDL with DDL learned
    Learned a topic about binding a dropdown list with another dropdown list using database primary key and foreign key relationship.

---
+++ BindDDLwithDDL.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BindDDLwithDDL.aspx.cs" Inherits="ASP_Tutorial.BindDDLwithDDL" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 182px;
+        }
+        .auto-style3 {
+            width: 182px;
+            height: 43px;
+        }
+        .auto-style4 {
+            height: 43px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <table cellpadding="4" cellspacing="4" class="auto-style1">
+                <tr>
+                    <td class="auto-style3">Country</td>
+                    <td class="auto-style4">
+                        <asp:DropDownList ID="DDL_Country" runat="server" Height="30px" Width="250px" OnSelectedIndexChanged="DDL_Country_SelectedIndexChanged" AutoPostBack="true">
+                        </asp:DropDownList>
+                        <asp:RequiredFieldValidator ID="DDL_Country_Required_Validator" runat="server" ErrorMessage="Country is required!" ForeColor="Red" Display="Dynamic" ControlToValidate="DDL_Country" InitialValue="Select Country"></asp:RequiredFieldValidator>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">City</td>
+                    <td>
+                        <asp:DropDownList ID="DDL_City" runat="server" Height="30px" Width="250px">
+                        </asp:DropDownList>
+                        <asp:RequiredFieldValidator ID="DDL_City_Required_Validator" runat="server" ErrorMessage="City is required!" ForeColor="Red" Display="Dynamic" ControlToValidate="DDL_City" InitialValue="Select City"></asp:RequiredFieldValidator>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">&nbsp;</td>
+                    <td>
+                        <asp:Button ID="Btn_Submit" runat="server" Height="36px" Text="Submit" Width="84px" onclick="Btn_Submit_Click"/>
+                    </td>
+                </tr>
+            </table>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ BindDDLwithDDL.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ASP_Tutorial
+{
+    public partial class BindDDLwithDDL : System.Web.UI.Page
+    {
+        static string cs = ConfigurationManager.ConnectionStrings["aspcoursecs"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                BindCountryDDL();
+            }
+        }
+
+        protected void BindCountryDDL()
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string query = "select * from Country";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            DDL_Country.DataSource = dt;
+            DDL_Country.DataTextField = "country_name";
+            DDL_Country.DataValueField = "country_id";
+            DDL_Country.DataBind();
+            DDL_Country.Items.Insert(0, "Select Country");
+        }
+
+        protected void BindCityDDL(int countryId)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string query = "select * from City where country_id = @country_id";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            sda.SelectCommand.Parameters.AddWithValue("@country_id", countryId);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            DDL_City.DataSource = dt;
+            DDL_City.DataTextField = "city_name";
+            DDL_City.DataValueField = "city_id";
+            DDL_City.DataBind();
+            DDL_City.Items.Insert(0, "Select City");
+        }
+
+        protected void DDL_Country_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int countryId = Convert.ToInt32(DDL_Country.SelectedValue);
+                BindCityDDL(countryId);
+            }
+            catch (Exception ex)
+            {
+
+                Response.Write("<script>alert('Country is required!');</script>");
+            }
+        }
+
+        protected void Btn_Submit_Click(object sender, EventArgs e)
+        {
+            Response.Write($"Selected Country is {DDL_Country.SelectedItem} and Selected City is {DDL_City.SelectedItem}. ");
+        }
+    }
+}
